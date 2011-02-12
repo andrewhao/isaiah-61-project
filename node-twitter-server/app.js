fa6ffe02ee7f907498a65e24af088123854a9ab0
Andrew Hao <andrewhao@gmail.com>
2011-02-12 00:22:01 -0800

    Added seed file of +/- injustice tweets. Sample of ~400 tweets. Hopefully can use this to test out a Bayesian filter. Added rudimentary Twitter search/filtering. Unfortunately, running into the issue that I cannot run multiple Twitter filter streams at once (duh). So might have to switch tactics and queue up Twitter searches as we go.

--- node-twitter-server/app.js
+++ node-twitter-server/app.js
@@ -1,9 +1,8 @@
-// twitter-node does not modify GLOBAL, that's so rude
-require.paths.unshift('~/.node_libraries');
 var sys = require('sys'),
     http = require('http'),
     express = require('express'),
     io = require('socket.io'),
+    util = require('util'),
     twitter = require('twitter');
 
 var server = express.createServer();
@@ -30,22 +29,49 @@ socket.on('connection', function(client){
             access_token_key: '12176642-TKySLSXlbR5mbbx0daBqtPtZKkUwxFlpA5GxY7u7a',
             access_token_secret: 'fcoeXCzFvZaDYScVyJRuqEpy5jC9SjS8U6V5aZkYptE'
     });
-
-    var track = "sad,depressed,afraid,trapped,lonely";
+    
+    var subject_kw = ["i feel", "i want", "i need", "i%27m"];
+    var pain_kw = ["sad","depressed","afraid","trapped","lonely","grief","cry"];
+    var pain_kw_reqd = ["feel"];
+    var grief_kw = ["passed","dead","death","gone","decease","dying","demise","grief"];
+    var oakland_kw = ["oakland"];
+    var justice_kw = ["injustice","justice","rape","violence","assault","murder","slavery","corruption","mugged"];
 
     /**
-     * Define stream behavior
+     * Do a twitter search
      */
-    twit.stream('statuses/filter', {track: track}, function(stream) {
+    twit.search(pain_kw.join(' OR ') + ' AND ' + pain_kw_reqd.join(' AND '), function(data) {
+        for (var i in data.results) {
+            sys.puts(sys.inspect(data.results[i].text));
+            client.send('searched and found: ' + sys.inspect(data.results[i].text));
+        }
+    })
+
+    /**
+     * Define stream behavior: Justice
+     */
+    twit.stream('statuses/filter', {track: grief_kw.concat(subject_kw).join(',')}, function(stream) {
         /**
          * Stream will log on data recv event.
          */
         stream.on('data', function (data) {
-                sys.puts(sys.inspect(data.text));
-                if (data.text.toLowerCase().indexOf('i\'m')  != -1 ||
-                    data.text.toLowerCase().indexOf('feel')  != -1) {
-                    client.send(data.text);
+                var out = data.text
+                var words = out.split(' ');
+                sys.puts(sys.inspect(out));                
+
+                // Rudimentary search.
+                var found = false;                
+                for (i in words) {
+                    var word = words[i];
+                    if (grief_kw.indexOf(word.toLowerCase()) != -1) {
+                        found = true;
+                        break;
+                    }
                 }
+                if (found) {
+                    client.send('pain: ' + out);
+                }
+                
         });
     });
     
