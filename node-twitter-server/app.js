e74e9b51cd3b3ff551b39bc896738ae7b5db0ca0
Andrew Hao <andrewhao@gmail.com>
2011-02-19 01:25:22 -0800

    Rearchitected to delegate program flow to an EventDriver class. EventDriver has instance variables involving the arduino and websocket. There's some sort of deadlock issue or something going on. Needs investigation.

--- node-twitter-server/app.js
+++ node-twitter-server/app.js
@@ -7,28 +7,42 @@ var sys = require('sys'),
     EE = require('events').EventEmitter,
     twitter = require('twitter');
 
+
 /**
- * QueueBoss
- * Drains from a set of queues in a (random?) and fair manner.
+ * Create a twitter stream to the Streaming API
  */
-function QueueBoss(queueList, webclient) {
+var twit = new twitter({
+    consumer_key: 'LsX6TUMSfwqqUaxfw4Kcsw',
+    consumer_secret: 'O9nD1g0Cuqngl5N1Ij1CoY0G9Nm96ZeOGr2CfQKSXQ',
+    access_token_key: '253928382-gEBAuXYl4jtezGUG7GbPhdxm0b0akm6raI2spd9Y',
+    access_token_secret: 'chLg4V2aTw6saRuoEmnLNFirr1Cq0g0rmQOcqy8Tw'
+});
+
+/**
+ * AggregateQueue
+ *
+ * A wrapper over multiple queues. Queues are drained in a 
+ * (random?) and fair manner.
+ */
+function AggregateQueue(queueList) {
     EE.call(this);
     this.queueList = queueList;
     this.currentIdx = 0;
-    this.client = webclient;
 }
-util.inherits(QueueBoss, EE);
+util.inherits(AggregateQueue, EE);
 
 // Shifts the queue, sets up the next one to be shifted next. 
 // TODO: randomize
-QueueBoss.prototype.drain = function() {
+AggregateQueue.prototype.shift = function() {
+    this.emit('shift');
     var data = this.queueList[this.currentIdx].shift();
-    this.client.send(data);
     this.currentIdx = (this.currentIdx + 1) % this.queueList.length;
+    return data;
 }
 
 /**
  * TweetQueue
+ *
  * A simple queue, modified to be evented and emit a "low"
  * event when under a threshold of n items.
  */
@@ -73,16 +87,96 @@ TweetQueue.prototype.checkLength = function() {
 }
 
 /**
- * Create a twitter stream to the Streaming API
+ * Setting up the AggregateQueue
  */
-var twit = new twitter({
-    consumer_key: 'LsX6TUMSfwqqUaxfw4Kcsw',
-    consumer_secret: 'O9nD1g0Cuqngl5N1Ij1CoY0G9Nm96ZeOGr2CfQKSXQ',
-    access_token_key: '253928382-gEBAuXYl4jtezGUG7GbPhdxm0b0akm6raI2spd9Y',
-    access_token_secret: 'chLg4V2aTw6saRuoEmnLNFirr1Cq0g0rmQOcqy8Tw'
-});
+ 
+// References to specific queues.
+var painQueue = new TweetQueue();
+var griefQueue = new TweetQueue();
+var justiceQueue = new TweetQueue();
+var oaklandQueue = new TweetQueue();
+
+// I don't want to see these.
+var blacklist = ['RT', 'bieb', 'justin'];
+
+var fullQueue = new AggregateQueue([painQueue, griefQueue, justiceQueue, oaklandQueue]);
+
+var queueSpec = {
+    // "bind up the brokenhearted"
+    pain: {
+        queue: painQueue,
+        kw: ["sad", "depressed", "afraid", "trapped","lonely","grief","cry"],
+        kw_req: "i feel",
+    },
+    // "oil of gladness instead of mourning"
+    grief: {
+        queue: griefQueue,
+        kw: ["passed away", "dead","death","decease","grief","RIP", "R.I.P.", "rest in peace"],
+        kw_req: "",
+    },
+    // "oaks of righteousness"
+    oakland: {
+        queue: oaklandQueue,
+        kw: ["oakland"],
+        kw_req: "",
+    },
+    // "day of vengeance of our God"
+    justice: {
+        queue: justiceQueue,
+        kw: ["injustice","justice","rape","violence","assault","murder","slavery","corruption","mugged"],
+        kw_req: "",
+    }
+};
+
+var subject_kw = ["i feel", "i want", "i need", "i%27m"];
+
+// Set up listeners for each queue.
+for (var qtype in queueSpec) {
+    // Module pattern
+    // http://meshfields.de/event-listeners-for-loop/
+    // Binds the queue spec to the scope in the closure.
+    (function(qtype) {
+        var qd = queueSpec[qtype];
+        qd.queue.on('low', function (queueLength) {
+            sys.puts('queue ' + qtype + ' is low! refilling...');
+            debugger;
+            if (!qd.queue.lock) {
+                // Set the lock.
+                qd.queue.lock = true;
+                /**
+                 * Do a twitter search
+                 */
+                twit.search(qd.kw_req + " (" + qd.kw.join(' OR ') + ")", {lang: 'en', rpp: 100}, function(data) {
+                    for (var i in data.results) {
+                        var text = data.results[i].text;
+                        
+                        // Filter out blacklisted elements
+                        var blacklisted = false;
+                        for (var i in blacklist) {
+                            var w = blacklist[i];
+                            if (text.toLowerCase().indexOf(w.toLowerCase()) != -1) {
+                                blacklisted = true;
+                                break;
+                            }
+                        }
+                        
+                        if (!blacklisted) {
+                            qd.queue.push(data.results[i]);
+//                            sys.puts(qtype + ' | pushing tweet: ' + sys.inspect(data.results[i]));
+//                            sys.puts(qtype + ' | new queue length: ' + qd.queue.queue.length)
+                        }
+                    }
+                });
+                // Undo the lock
+                qd.queue.lock = false;
+            }
+        });
+    }) (qtype);
+}
     
-// Simple web server
+/**
+ * Simple web server
+ */
 var server = express.createServer();
 
 server.configure(function() {
@@ -100,22 +194,18 @@ server.listen(8080);
 /**
  * Socket interface to Arduino script.
  */
-var dummy_i = 0;
 var pins = [3, 5, 6, 9]
+
 var arduino = net.createServer(function (stream) {
+    
+    // Set a reference to the arduino stream from the socket.
+    boss.arduino_socket = stream;
+    
     stream.setEncoding('utf8');
     stream.on('connect', function() {
-        //stream.write('Connected to twitter server.\0');
         sys.puts('Connection detected from Arduino: ' + sys.inspect(stream, false));
-        
-        // Test function: send a byte-command every second.
-        // Command is a byte terminated by a NULL.
-        setInterval(function () {
-            sys.puts('Sending dummy i: ' + dummy_i);
-            stream.write(pins[dummy_i] + '\0');
-            dummy_i = (dummy_i + 1) % 4;
-        }, 5000)
     });
+    
     stream.on('data', function(data) {
         sys.puts('Echoing data from arduino client: ' + sys.inspect(data));
         stream.write('Echo: ' + data + '\0');
@@ -126,120 +216,20 @@ var arduino = net.createServer(function (stream) {
 });
 arduino.listen(7000);
 
-
 /**
  * Socket.io
  */
 var websocket = io.listen(server); 
 websocket.on('connection', function(client){    
-    var painQueue = new TweetQueue();
-    var griefQueue = new TweetQueue();
-    var justiceQueue = new TweetQueue();
-    var oaklandQueue = new TweetQueue();
     
-    // I don't want to see these.
-    var blacklist = ['RT', 'bieb', 'justin'];
-    
-    var queueBoss = new QueueBoss([painQueue, griefQueue, justiceQueue, oaklandQueue], client);
-    
-    var queueSpec = {
-        pain: {
-            queue: painQueue,
-            kw: ["sad","depressed","afraid","trapped","lonely","grief","cry"],
-            kw_req: "i feel",
-        },
-        grief: {
-            queue: griefQueue,
-            kw: ["passed away", "dead","death","decease","grief","RIP", "R.I.P.", "rest in peace"],
-            kw_req: "",
-        },
-        oakland: {
-            queue: oaklandQueue,
-            kw: ["oakland"],
-            kw_req: "",
-        },
-        justice: {
-            queue: justiceQueue,
-            kw: ["injustice","justice","rape","violence","assault","murder","slavery","corruption","mugged"],
-            kw_req: "",
-        }
-    };
-    
-    var subject_kw = ["i feel", "i want", "i need", "i%27m"];
-
-    setInterval(function() {
-        queueBoss.drain();
-    }, 5000)
-    
-    // Set up listeners for each queue.
-    for (var qtype in queueSpec) {
-        // Module pattern
-        // http://meshfields.de/event-listeners-for-loop/
-        // Binds the queue spec to the scope in the closure.
-        (function(qtype) {
-            var qd = queueSpec[qtype];
-            qd.queue.on('low', function (queueLength) {
-                if (!qd.queue.lock) {
-                    // Set the lock.
-                    qd.queue.lock = true;
-                    /**
-                     * Do a twitter search
-                     */
-                    twit.search(qd.kw_req + " (" + qd.kw.join(' OR ') + ")", {lang: 'en', rpp: 30}, function(data) {
-                        for (var i in data.results) {
-                            var text = data.results[i].text;
-                            
-                            var blacklisted = false;
-                            for (var i in blacklist) {
-                                var w = blacklist[i];
-                                if (text.toLowerCase().indexOf(w.toLowerCase()) != -1) {
-                                    blacklisted = true;
-                                    break;
-                                }
-                            }
-                            
-                            if (!blacklisted) {
-                                qd.queue.push(data.results[i]);
-                                sys.puts(qtype + ' | pushing tweet: ' + sys.inspect(data.results[i]));
-                                sys.puts(qtype + ' | new queue length: ' + qd.queue.queue.length)
-                            }
-                        }
-                    });
-                    // Undo the lock
-                    qd.queue.lock = false;
-                }
-            });
-        }) (qtype);
-    }
-            
-    /**
-     * Define stream behavior: Justice
-     */
-//    twit.stream('statuses/filter', {track: grief_kw.concat(subject_kw).join(',')}, function(stream) {
-        /**
-         * Stream will log on data recv event.
-         */
-/*        stream.on('data', function (data) {
-                var out = data.text
-                var words = out.split(' ');
-                sys.puts(sys.inspect(out));                
-
-                // Rudimentary search.
-                var found = false;                
-                for (i in words) {
-                    var word = words[i];
-                    if (grief_kw.indexOf(word.toLowerCase()) != -1) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (found) {
-                    //client.send('pain: ' + out);
-                }
-                
-        });
-    });
-  */  
+    // Give the EventController a reference to client
+    boss.web_socket = client;
+/*    
+    setInterval(function () {
+        var tweet = fullQueue.shift()
+        sys.puts('tweet is: ' + tweet);
+    }, 10000);
+*/
     // new client is here! 
     client.on('message', function(data){
         sys.puts(sys.inspect('client said:' + data));
@@ -248,3 +238,52 @@ websocket.on('connection', function(client){
         sys.puts(sys.inspect('client disconnected.'))
     });
 });
+
+/**
+ * The brains of the whole operation
+ *
+ * This is responsible for setting up connections to web and arduino.
+ */
+function EventDriver() {
+    EE.call(this);
+    this.web_socket = null;
+    this.arduino_socket = null;
+    this.dummy_i = 0;
+}
+util.inherits(EventDriver, EE);
+
+EventDriver.prototype.writeArduino = function() {
+    // Test function: send a byte-command. Wait for ack before sending another.
+    sys.puts('Sending dummy i: ' + this.dummy_i);
+    this.arduino_socket.write(pins[this.dummy_i] + '\0');
+    this.dummy_i = (this.dummy_i + 1) % 4;
+}
+
+/**
+ * Checks whether we're properly initialized.
+ */
+EventDriver.prototype.isReady = function() {
+    return this.web_socket != null && 
+           this.arduino_socket != null;
+}
+
+/**
+ * Event loop.
+ */
+EventDriver.prototype.tick = function() {
+    sys.puts('tick');
+    // Drain the queue and grab the newest tweet to send.
+    var tweet = fullQueue.shift();
+    if (tweet) {
+        this.web_socket.send(tweet);
+        this.writeArduino();
+    }
+}
+
+boss = new EventDriver();
+
+setInterval(function() {
+    if (boss.isReady()) {
+        boss.tick();
+    }
+}, 1000);
--- python-arduino-client/arduino.py
+++ python-arduino-client/arduino.py
@@ -3,16 +3,34 @@ import firmata
 import time
 import threading
 
-class FaderController():
-    """Manages Fader threads"""
-    def fade(self, pinNum):
-        pass
 
 lowerBound = 60
 upperBound = 255
 
+# --------------------------------------------------------------
+# Faders, etc
+# --------------------------------------------------------------
+curFader = None
+
+# Fader flag acts as messaging system for active Fader object.
+# setting the flag to True should effectively kill the Fader thread
+stopFaderFlag = False
+faderFlagLock = threading.Lock()
+intentToChangeLock = threading.Lock()
+
+faderFlag = threading.Event()
+
+
 # Three pulses.
 pinIntensities = range(0, upperBound) + \
+                range(upperBound, lowerBound - 1, -1) + \
+                range(lowerBound, upperBound + 1) + \
+                range(upperBound, lowerBound - 1, -1) + \
+                range(lowerBound, upperBound + 1) + \
+                range(upperBound, lowerBound - 1, -1) + \
+                range(lowerBound, upperBound + 1) + \
+                range(upperBound, lowerBound - 1, -1) + \
+                range(lowerBound, upperBound + 1) + \
                 range(upperBound, lowerBound - 1, -1) + \
                 range(lowerBound, upperBound + 1) + \
                 range(upperBound, lowerBound - 1, -1) + \
@@ -26,20 +44,58 @@ class Fader(threading.Thread):
         threading.Thread.__init__(self)
         self.pinNum = pinNum
 
+    def stopFader(self):
+        global stopFaderFlag
+        """Protects access to the fader flag."""
+        print '-%s- stopping fader. val: %s' % (self.pinNum, stopFaderFlag)
+        faderFlagLock.acquire()
+        print '-%s- stopping fader freal: %s' % (self.pinNum, stopFaderFlag)
+        stopFaderFlag = True
+        print '-%s- just changed: %s' % (self.pinNum, stopFaderFlag)
+        faderFlagLock.release()
+        print '-%s- again, with released lock: %s' % (self.pinNum, stopFaderFlag)
+
+    def resetFaderFlag(self):
+        """
+        Once a fader is stopped, we should reset it. This is called only
+        from a stopping Fader.
+        """
+        global stopFaderFlag
+        print '-%s- resetting fader' % self.pinNum
+        faderFlagLock.acquire()
+        stopFaderFlag = False
+        faderFlagLock.release()
+
     def run(self):
         """
         Run the glow effect for 2 cycles and turn off.
         """
+        global stopFaderFlag
+        print 'running the fader on pin %s' % self.pinNum
         for i in pinIntensities:
-            a.analog_write(int(self.pinNum), i)
-            time.sleep(0.003)
-        
+            
+            print '-%s- stopFaderFlag is %s' % (self.pinNum, stopFaderFlag)
+            
+            # Check the flag to see if we should exit.
+            if stopFaderFlag:
+                print '-%s- declared intent to stop' % self.pinNum
+                self.resetFaderFlag()
+                print '-%s- stopping fader on pin %s' % (self.pinNum, self.pinNum)
+                print '-%s- after I stop, fader flag is %s' % (self.pinNum, stopFaderFlag)
+                # one line after the return, the thread should die.
+                return
+            else:
+                print 'nah'
+                print '-%s-- stopFaderFlag is %s' % (self.pinNum, stopFaderFlag)
+                a.analog_write(int(self.pinNum), i)
+                time.sleep(0.003)
+        print '+%s+ fader was allowed to fade out on pin %s' % (self.pinNum, self.pinNum)
 
 # Set up the Arduino interfaces.
 serial_addr = '/dev/tty.usbmodem3d11'
 a = firmata.Arduino(serial_addr)
-a.pin_mode(3, firmata.PWM)
 print ('Connecting to bootloader. Please wait.')
+# Need 2s to make sure the connection is made with the firmware.
 a.delay(2)
 
 # Set up the socket interface to nodejs server.
@@ -48,21 +104,32 @@ port = 7000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((hostname, port))
 
+
 # Listen on socket and drain when it's time.
 #
 # HACK! A single driver loop both controls the fade function on the LED pin,
 # and drains the socket and switches pinouts.
 while True:
     # read 2-byte pin command at a time (pin + NULL).
+    # 
     data = s.recv(8);
-    if data:
-        print 'all data recv was: %s' % data
-        pinNum = data[0]
-        print 'pin to pulse is: %s' % pinNum
-        f = Fader(pinNum)
-        f.start()
-#        a.analog_write(cur_pin, 0)
-#        cur_pin = int(data[0:-1])
-#        a.analog_write(cur_pin, 254)
-    else:
-        print 'no data recv'
+#    assert data, 'No data found on socket'
+    if not data:
+        continue
+
+    print 'all data recv was: %s' % data
+    assert len(data) == 2, 'Only two bytes expected on socket queue.'
+
+    pinNum = data[0]
+    print 'pin to pulse is: %s' % pinNum
+    
+    # stop the current fader
+    if curFader:
+        curFader.stopFader()
+
+    print 'before I start a new Fader, the current state of fader flag is: %s' % stopFaderFlag
+    curFader = Fader(pinNum)
+    curFader.daemon = True
+
+    # Start the fader on that pin.
+    curFader.start()
